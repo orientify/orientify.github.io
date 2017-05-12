244ea1bacd432ac9b1137363ed520503d599b091
orientify <nuustikcat@gmail.com>
2017-05-12 22:51:55 +0300

    f

--- javascripts/writeTrackInfo.js
+++ javascripts/writeTrackInfo.js
@@ -1,5 +1,6 @@
 var mapName;
 $(function() {
+    giveNextTrackNameNumber();
     fileButton.addEventListener('change', function(e){
         var file = e.target.files[0];
         var storageRef = firebase.storage().ref('maps/' + file.name);
@@ -9,46 +10,64 @@ $(function() {
 
     });
 
-    
-
     document.getElementById("logOut").onclick = signOutFromGoogle;
     console.log("tere");
-
-    document.getElementById('fileButton').onchange = function () {
-        alert('Selected file: ' + this.value);
-        console.log(this.value);
-    };
 });
 
+
 function writeTrackInfo() {
-        var rada = document.getElementById('rada').value;
-        var linn = document.getElementById('linn').value;
+    var rada = document.getElementById('rada').value;
+    var linn = document.getElementById('linn').value;
+    var length = document.getElementById('length').value;
+    var checkpoints = [];
+    var answer = [];
+    var choice = [];
+    var time = [];
+    console.log(giveNextTrackNameNumber());
+    for(var i = 1; i < 3; i++){
+        checkpoints[i] = i;
+        answer[i] = document.getElementById('answer' + i).value;
+        choice[i] = document.getElementById('choice' + i).value;
+        time[i] = document.getElementById('time' + i).value;
+    }
 
-        var checkpoints = [];
-        var answer = [];
-        var choice = [];
-        var time = [];
+    if(rada.length >0){
+        firebase.database().ref('tracksNames/' + giveNextTrackNameNumber()).set({
+            city: linn,
+            name: rada
+        });
+        firebase.database().ref('trackss/' + giveNextTrackNameNumber()).set({
+            length: length,
+            map: mapName
+        });
         for(var i = 1; i < 3; i++){
-            checkpoints[i] = document.getElementById('checkpoints' + i).value;
-            answer[i] = document.getElementById('answer' + i).value;
-            choice[i] = document.getElementById('choice' + i).value;
-            time[i] = document.getElementById('time' + i).value;
-        }
-
-        if(rada.length >0){
-            firebase.database().ref('trackss/' + rada).set({
-                city: linn,
-                map: mapName
+            console.log('trackss/' + giveNextTrackNameNumber() + '/checkpoints/' + checkpoints[i]);
+            firebase.database().ref('trackss/' + giveNextTrackNameNumber() + '/checkpoints/' + checkpoints[i]).set({
+                answer: answer[i],
+                choice: choice[i],
+                time: time[i]
             });
-            for(var i = 1; i < 3; i++){
-                console.log('trackss/' + rada + '/checkpoints/' + checkpoints[i]);
-                firebase.database().ref('trackss/' + rada + '/checkpoints/' + checkpoints[i]).set({
-                    answer: answer[i],
-                    choice: choice[i],
-                    time: time[i]
-                });
-            }
+            
         }
     }
+}
+function giveNextTrackNameNumber(){
+    var trackCountRef = firebase.database().ref('trackNames/');
+    var count = 1;
+    trackCountRef.on('value', function(snapshot) {
+        var tracks = snapshot.val();
+        for (var track in tracks) {
+            count++;
+        }
+    });
+    console.log("tere");
+    return count;
+}
+
+function sleep(delay) {
+    var start = new Date().getTime();
+    while (new Date().getTime() < start + delay);
+}
+
 
 
